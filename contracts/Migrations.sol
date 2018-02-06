09757e4c086752e19cd176fdf89f61cf7b21149d
gabriel <777canaan@gmail.com>
2018-02-07 09:37:14 +1300

    fix truffle.js and migrations

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
-pragma solidity ^0.4.17;
-=======
+
 pragma solidity ^0.4.4;
->>>>>>> 277891d51ee45eeb250f8a32a3c32b9ed1aafa21
+
 
 contract Migrations {
   address public owner;
@@ -12,18 +10,7 @@ contract Migrations {
     if (msg.sender == owner) _;
   }
 
-<<<<<<< HEAD
   function Migrations() public {
-    owner = msg.sender;
-  }
-
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
-
-  function upgrade(address new_address) public restricted {
-=======
-  function Migrations() {
     owner = msg.sender;
   }
 
@@ -32,7 +19,6 @@ contract Migrations {
   }
 
   function upgrade(address new_address) restricted {
->>>>>>> 277891d51ee45eeb250f8a32a3c32b9ed1aafa21
     Migrations upgraded = Migrations(new_address);
     upgraded.setCompleted(last_completed_migration);
   }
